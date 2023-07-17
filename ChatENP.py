66ad16f3462910117c5667deeece42756b416f07
Luis M. Barrera <131980810+lubauss@users.noreply.github.com>
2023-07-17 18:01:14 +0200

    Update ChatENP.py
    GPT-3.5-turbo

--- ChatENP.py
+++ ChatENP.py
@@ -13,8 +13,6 @@ import os
 import hashlib
 from streamlit_chat import message
 
-from langchain.agents import initialize_agent, Tool
-from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
@@ -43,7 +41,7 @@ def tiktoken_len(text):
     return len(tokens)
 
 # Set the chunk size and overlap
-chunk_size = 700
+chunk_size = 500
 chunk_overlap = 100
 
 # Create the text splitter
@@ -145,50 +143,22 @@ def get_vectorstore(query_namespace, index):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-    llm_1 = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+    llm_1 = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     # Retrieval qa chain
     qa = RetrievalQAWithSourcesChain.from_chain_type(
         llm=llm_1,
         chain_type="stuff",
         retriever=vectorstore.as_retriever(),
-        return_source_documents=True
+        return_source_documents=True,
+    
     )
     return qa
 
-def run_qa(qa, query):
+def handle_chat(qa, query):
     result = qa({"question":query})
-    return f"{result['answer']}\n Sources:\n {result['sources']}"
-
-def get_agent(run_qa_func):
-    llm_2 = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
-    
-    # Calculator
-    #llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
-    
-    tools = [
-        Tool(
-            name='Knowledge Base',
-            func=run_qa_func,
-            description="use this tool when answering general knowledge queries to get more information about the topic",
-            return_direct=True,
-        ),
-
-    ]
-   
-    # Conversational memory
-    conversational_memory = ConversationBufferWindowMemory(
-        memory_key='chat_history',
-        k=7,
-        return_messages=True
-    )
-    agent = initialize_agent(tools, llm_2, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, max_iterations=3,early_stopping_method='generate', memory=conversational_memory,handle_parsing_errors=True)
-
-    return agent
-
-def handle_chat(agent, query):
-    response = agent.run(input=query)
-    st.session_state["agent"] = agent
+    response = f"{result['answer']}\n Sources:\n {result['sources']}"
+    st.session_state["qa"] = qa
     st.session_state.requests.append(query)
     st.session_state.responses.append(response)
     
@@ -218,7 +188,7 @@ def main():
                     # create vector store
                     pinecone_index(upload_namespace, docs_pages, index_name)
         # Always show the header
-        st.subheader("Haz clic 👆 en la base de datos 🗄️ que quieras consultar 🔍")
+        st.subheader("👆 Haz clic  en la base de datos 🗄️ que quieras consultar 🔍")
 
         pinecone.init(api_key=os.environ['PINECONE_API_KEY'], 
                 environment=os.environ['PINECONE_API_ENV'])
@@ -239,16 +209,17 @@ def main():
                 st.session_state['namespace'] = namespace
                 # Update query_namespace
                 query_namespace = st.session_state['namespace']
-        # Initialize the agent only if it doesn't exist in the session state or the namespace has changed
-        if 'agent' not in st.session_state or st.session_state['namespace'] != query_namespace:
-            # Initialize your agent here
+
+        # Initialize the qa_chain only if it doesn't exist in the session state or the namespace has changed
+        if 'qa_chain' not in st.session_state or st.session_state['namespace'] != query_namespace:
+            # Initialize your qa_chain here
             query_namespace = st.session_state['namespace']
             vectorstore = get_vectorstore(query_namespace, index)        
-            qa = get_conversation_chain(vectorstore)
-            agent = get_agent(lambda query: run_qa(qa, query)) # Pass run_qa as a lambda function
-            st.session_state['agent'] = agent # Set the agent here
+            qa_chain = get_conversation_chain(vectorstore)
+
+            st.session_state['qa_chain'] = qa_chain # Set the qa_chain here
         else:
-            agent = st.session_state['agent'] # If the agent already exists in the session state, get it.
+            qa_chain = st.session_state['qa_chain'] # If the qa_chain already exists in the session state, get it.
 
 
         # Check if the namespace has been selected
@@ -281,7 +252,7 @@ def main():
 
         if send_button and query:
             with st.spinner("Buscando... 🔎"):
-                handle_chat(agent, query)
+                handle_chat(qa_chain, query)
 
             # Add a small delay
             sleep(0.1)
